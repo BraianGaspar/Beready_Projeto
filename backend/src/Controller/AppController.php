e342a5a60b646d25b511f2a1c07fa947b435b04f
Braian Gaspar Da Rosa <braian_gaspar@hotmail.com>
2026-04-05 19:08:06 -0300

    Correções de erro do DebugKit

--- backend/src/Controller/AppController.php
+++ backend/src/Controller/AppController.php
@@ -12,14 +12,26 @@ class AppController extends Controller
     {
         parent::initialize();
         
-        $this->autoRender = false;
+        // Configura CORS
+        $this->response = $this->response->withHeader('Access-Control-Allow-Origin', '*');
+        $this->response = $this->response->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $this->response = $this->response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         
-        // Configura para JSON
-        $this->response = $this->response->withHeader('Content-Type', 'application/json');
+        // Força JSON
+        $this->response = $this->response->withType('application/json');
+        
+        // Desabilita views
+        $this->autoRender = false;
     }
 
     public function beforeFilter(EventInterface $event): void
     {
         parent::beforeFilter($event);
+        
+        // Responde OPTIONS - NÃO RETORNA NADA, apenas modifica a response
+        if ($this->request->is('options')) {
+            $this->response = $this->response->withStatus(200);
+            $this->response = $this->response->withStringBody('');
+        }
     }
 }
